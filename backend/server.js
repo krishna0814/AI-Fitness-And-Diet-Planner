a2c7adaa35bf2029959341c5714cd30766d3ff2b
Krishna Maurya <krishnamaurya914068@gmail.com>
2026-05-07 10:26:23 +0530

    Fixed auth route

--- backend/server.js
+++ backend/server.js
@@ -67,7 +67,7 @@ mongoose.connect(process.env.MONGO_URI)
     app.listen(PORT, () => {
 
       console.log(
-        `🚀 Server running on port ${PORT}`
+        `🚀 Server running on ${PORT}`
       );
 
     });
